2f2c921e8c06571dac3fbdc244728110a74067d6
assmadaaif <assmadaaif@gmail.com>
2022-02-27 17:19:49 +0100

    footer

--- include/footer.php
+++ include/footer.php
@@ -1,12 +1,6 @@
 <footer class="main-footer">
-    <div class="pull-right d-none d-sm-inline-block">
-        <ul class="nav nav-primary nav-dotted nav-dot-separated justify-content-center justify-content-md-end">
-		  <li class="nav-item">
-			<a class="nav-link" href="#" target="_blank">Purchase Now</a>
-		  </li>
-		</ul>
-    </div>
-	  &copy; <script>document.write(new Date().getFullYear())</script> <a href="https://www.multipurposethemes.com/">Multipurpose Themes</a>. All Rights Reserved.
+   
+	  &copy; <script>document.write(new Date().getFullYear())</script> <a href="https://www.hiziconnect.com/">Hiziconnect</a>
   </footer>
   <!-- Side panel -->   
   <!-- quick_user_toggle -->
